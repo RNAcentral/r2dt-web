f4619f8ad8f8ec6449755645730a30d761644347
carlosribas <caduribas@gmail.com>
2023-08-22 16:08:10 +0100

    Add route to RNAcanvas server

--- src/services/routes.jsx
+++ src/services/routes.jsx
@@ -2,6 +2,7 @@ let ebiDevOrProd = process.env.REACT_APP_BRANCH === 'dev' ? 'wwwdev' : 'www';
 let server =  `https://${ebiDevOrProd}.ebi.ac.uk/Tools/services/rest/r2dt`;
 let firebaseServer = process.env.REACT_APP_FIREBASE;
 let rnacentralServer = process.env.REACT_APP_BRANCH === 'dev' ? 'test.rnacentral' : 'rnacentral';
+let rnaCanvasServer = process.env.REACT_APP_BRANCH === 'dev' ? 'https://rna2drawer2-dev.uk.r.appspot.com' : 'https://rnacanvas.app';
 
 module.exports = {
   submitJob:  () => `${server}/run`,
@@ -11,5 +12,6 @@ module.exports = {
   fetchTsv:   (jobId) => `${server}/result/${jobId}/tsv`,
   firebase:   () => `${firebaseServer}/data.json`,
   firebaseId: (id) => `${firebaseServer}/data/${id}.json`,
-  fetchUrs:   (urs) => `https://${rnacentralServer}.org/api/v1/rna/${urs}/2d/`
+  fetchUrs:   (urs) => `https://${rnacentralServer}.org/api/v1/rna/${urs}/2d/`,
+  rnaCanvas:  (jobId) => `${rnaCanvasServer}?rna_2d_schema_url=${server}/result/${jobId}/json`
 };
