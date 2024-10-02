edb3f6fca032be9503c8a0ce23cc5974e8a81db1
carlosribas <caduribas@gmail.com>
2024-10-02 20:53:16 +0100

    #14 - Disable fields when using dot-bracket notation

--- src/containers/R2DT/components/SearchForm/components/Advanced/index.jsx
+++ src/containers/R2DT/components/SearchForm/components/Advanced/index.jsx
@@ -8,6 +8,7 @@ import { FiExternalLink } from 'react-icons/fi';
 
 class Advanced extends React.Component {
   render() {
+    const dotBracket = /[.()]/.test(this.props.sequence);
     const fixCss = this.props.customStyle && this.props.customStyle.fixCss && this.props.customStyle.fixCss === "true" ? "1.5rem" : "";
     return (
       <div className="card">
@@ -27,20 +28,20 @@ class Advanced extends React.Component {
                   <div className="row mb-2 form-group">
                     <div className="col-12 col-sm-6">
                       <div className="form-check">
-                        <input className="form-check-input" type="radio" name="radios" id="radio1" value="option1" checked={ this.props.searchMethod === 'option1' } onChange={ (e) => this.props.handleOptionChange(e) }/>
+                        <input className="form-check-input" type="radio" name="radios" id="radio1" value="option1" checked={ this.props.searchMethod === 'option1' } onChange={ (e) => this.props.handleOptionChange(e) } disabled={dotBracket ? "disabled" : ""}/>
                         <label className="form-check-label" htmlFor="radio1">Browse all templates</label>
                       </div>
                     </div>
                     <div className="col-12 col-sm-6">
                       <div className="form-check">
-                        <input className="form-check-input" type="radio" name="radios" id="radio2" value="option2" checked={ this.props.searchMethod === 'option2' } onChange={ (e) => this.props.handleOptionChange(e) }/>
+                        <input className="form-check-input" type="radio" name="radios" id="radio2" value="option2" checked={ this.props.searchMethod === 'option2' } onChange={ (e) => this.props.handleOptionChange(e) } disabled={dotBracket ? "disabled" : ""}/>
                         <label className="form-check-label" htmlFor="radio2">Type to find a template</label>
                       </div>
                     </div>
                   </div>
                   {
                     this.props.searchMethod === "option1" ?
-                        <select style={{fontSize: fixCss}} className="form-control" value={this.props.templateId} onChange={(e) => this.props.onChangeTemplateId(e)}>
+                        <select style={{fontSize: fixCss}} className="form-control" value={this.props.templateId} onChange={(e) => this.props.onChangeTemplateId(e)} disabled={dotBracket ? "disabled" : ""}>
                           <option key="default" value="">Select a template</option>
                           {templates.map((item) => (
                             <option key={item.model_id} value={item.model_id}>
@@ -57,7 +58,8 @@ class Advanced extends React.Component {
                           paginate={false}
                           onChange={(e) => this.props.onChangeTemplateId(e)}
                           inputProps={{style: { fontSize: fixCss } }}
-                          emptyLabel={'No templates found'} >
+                          emptyLabel={'No templates found'}
+                          disabled={dotBracket} >
                           {({ onClear, selected }) => (
                             <div className="rbt-aux">
                               {!!selected.length && <ClearButton onClick={onClear} />}
@@ -79,13 +81,13 @@ class Advanced extends React.Component {
                   <div className="row mb-2">
                     <div className="col-12">
                       <div className="form-check">
-                        <input className="form-check-input" type="checkbox" id="constrainedFolding" value={this.props.constrainedFolding} onChange={() => this.props.onChangeConstrainedFolding()} />
+                        <input className="form-check-input" type="checkbox" id="constrainedFolding" value={this.props.constrainedFolding} onChange={() => this.props.onChangeConstrainedFolding()} disabled={dotBracket ? "disabled" : ""}/>
                         <label className="form-check-label" htmlFor="constrainedFolding">Constrained folding</label>
                       </div>
                     </div>
                   </div>
                   {
-                    this.props.constrainedFolding ? <select style={{fontSize: fixCss}} className="form-control" value={this.props.foldType} onChange={(e) => this.props.onChangeFoldType(e)}>
+                    this.props.constrainedFolding ? <select style={{fontSize: fixCss}} className="form-control" value={this.props.foldType} onChange={(e) => this.props.onChangeFoldType(e)} disabled={dotBracket ? "disabled" : ""}>
                       <option key="auto" value="">Auto</option>
                       <option key="full_molecule" value="full_molecule">Full molecule</option>
                       <option key="insertions_only" value="insertions_only">Insertions only</option>
@@ -110,6 +112,7 @@ class Advanced extends React.Component {
 const mapStateToProps = (state) => ({
   templateId: state.templateId,
   searchMethod: state.searchMethod,
+  sequence: state.sequence,
   constrainedFolding: state.constrainedFolding,
   foldType: state.foldType,
 });
