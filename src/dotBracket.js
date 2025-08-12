3b4f273b6f63c2d844b0aabb2250111a84181d7f
caduribas <caduribas@gmail.com>
2025-08-11 21:05:05 -0300

    Create the r2dtDotBracket function

---
+++ src/dotBracket.js
@@ -0,0 +1,8 @@
+export function r2dtDotBracket(dotBracketNotation) {
+    return `
+        <div class="r2dt-card">
+            <div class="r2dt-card-header"><strong>Dot-bracket notation</strong></div>
+            <div class="r2dt-card-body"><span>${dotBracketNotation}</span></div>
+        </div>
+    `;
+}
