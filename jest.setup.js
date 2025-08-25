4b1c294ec3e5223528ca2f6151df3dea4f7b2848
caduribas <caduribas@gmail.com>
2025-08-25 20:21:57 -0300

    Silence console.error and console.warn during tests

--- jest.setup.js
+++ jest.setup.js
@@ -10,3 +10,7 @@ window.ResizeObserver = ResizeObserver;
 
 // Provide a safe default fetch mock
 global.fetch = jest.fn();
+
+// Silence console.error and console.warn during tests
+jest.spyOn(console, 'error').mockImplementation(() => {});
+jest.spyOn(console, 'warn').mockImplementation(() => {});
